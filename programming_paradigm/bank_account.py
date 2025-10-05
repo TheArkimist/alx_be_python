1442f64812aa1681d838c027891f16f7fddb91c5
Ci$c0 <emmanueloduro37@gmail.com>
2025-10-05 10:34:01 +0000

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -23,4 +23,4 @@ class BankAccount:
 
     def display_balance(self):
         """Print the current balance."""
-        print(f"Current balance: {self.account_balance:.2f}")
+        print("Current balance: ", self.account_balance:.2f)
