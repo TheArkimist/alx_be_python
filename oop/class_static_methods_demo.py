86e0987f88321efe132b7ea7033896d22db74174
Ci$c0 <emmanueloduro37@gmail.com>
2025-10-12 00:40:30 +0000

    Create class_static_methods_demo.py

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,12 @@
+class Calculator:
+    # Class attribute
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {Calculator.calculation_type}")
+        return a * b
